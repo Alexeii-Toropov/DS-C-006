103f5bd00bde564018f1e088f98733b192c4a9a5
Alexeii Toropov <leha.toropov.04@mail.ru>
2022-08-28 22:53:41 +0300

    fin

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿Console.Write("Введите кол-во элементов в массиве: ");
+int M = Convert.ToInt32(Console.ReadLine());
+int[] array = new int[M];
+for (int i = 0; i < M; i++)
+{
+    Console.Write("Введите число: ");
+    array[i] = Convert.ToInt32(Console.ReadLine());
+}
+int count = 0;
+for (int j = 0; j < array.Length; j++)
+{
+    if (array[j] > 0)
+    {
+        count += 1;
+    }
+}
+Console.WriteLine("Кол-во чисел в массиве > 0 =  " + count);
